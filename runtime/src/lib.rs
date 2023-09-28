2e44b229c259a48f483eaf1b54bdff151931c1b9
Tlalocman <desneruda@gmail.com>
2023-09-28 15:54:00 -0600

    🐛 fix(lib.rs): fix typo in spec_version field assignment ✨ feat(lib.rs): add support for Chaos spec version 154

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -133,7 +133,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
 	// Chaos spec version
-	spec_version: 154
+	spec_version: 154,
 	// Inova spec version
 	// spec_version: 168,
 	impl_version: 1,
